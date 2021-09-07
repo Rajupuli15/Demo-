305085d75dc4aaa3a1406d1175977df082b3fbc9
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-07 01:33:27 -0400

    Added:Hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args{
-        System.out.println("welcome to Demo class");
+        System.out.println("New welcome to Demo class");
+        System.out.println("Hello World");
     }
 }
